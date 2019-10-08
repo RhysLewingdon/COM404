e009b424cebec98d637e74b57136c6ad1cf9f5d3
RhysLewingdon <4lewir71@solent.ac.uk>
2019-10-08 09:41:58 +0000

    simple for loop

---
+++ 3.5-IterationFOR/simpleloop.py
@@ -0,0 +1,10 @@
+number = int(input("How many mountains should I display? "))
+print("Displaying...")
+
+for count in range(number):
+    print("            --          ")
+    print("           /  \_        ")
+    print("          /^    \       ")
+    print("         /  ^    \_     ")
+    print("       _/ ^ ^     ^\    ")
+    print("      /  ^     ^    \   ")
