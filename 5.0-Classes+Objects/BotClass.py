6c3f2180177e9a3f6e4e4f553140f66e1470ae74
RhysLewingdon <4lewir71@solent.ac.uk>
2019-11-05 10:44:10 +0000

    a

--- 5.0-Classes+Objects/BotClass.py
+++ 5.0-Classes+Objects/BotClass.py
@@ -6,24 +6,27 @@ class Bot:
         self.shield = shield
 
     def display_name(self):
-        print("The name of the bot is: "+self.name)
+        print("The name of the bot is: \n"+self.name)
 
     def display_age(self):
-        print("The age of the bot is: "+str(self.age))
+        print("The age of the bot is: \n"+str(self.age))
 
     def display_energy(self):
-        print("The bot energy level is at: "+str(self.energy))
+        print("The bot energy level is at: \n"+str(self.energy))
 
     def display_shield(self):
-        print("The bot shield level is at: "+str(self.shield))
+        print("The bot shield level is at: \n"+str(self.shield))
 
     def display_summary(self):
-        print("The Bot '{}' is {} years old and has {} energy and {} shield.".format(self.name, self.age, self.energy, self.shield))
+        print("The Bot \n{}\n is \n{}\n years old and has \n{}\n energy and \n{}\n shield.".format(self.name, self.age, self.energy, self.shield))
 
     def __str__(self):
         return 'Bot(name='+self.name+', age='+str(self.age)+', energy='+str(self.energy)+', shield='+str(self.shield) + ')'
 
-Bot1 = Bot("Bot1", 18, 90, 30)
+Bot1 = Bot("     _______  _______  _______  ____  \n    |  _    ||       ||       ||    | \n    | |_|   ||   _   ||_     _| |   | \n    |       ||  | |  |  |   |   |   | \n    |  _   | |  |_|  |  |   |   |   | \n    | |_|   ||       |  |   |   |   | \n    |_______||_______|  |___|   |___| \n",
+ 18,
+  90,
+   30)
 
 Bot1.display_name()
 print("#################################")
