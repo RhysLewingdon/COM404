c82b3ffd33150cfa69da6ca3388190dfdc18aef5
Rhys Lewingdon <4lewir71@solent.ac.uk>
2019-12-03 11:23:25 +0000

    changes

--- 7.0-images/workingwithimages.py
+++ 7.0-images/workingwithimages.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 class Gui(Tk):
 
@@ -21,7 +22,7 @@ class Gui(Tk):
 
     def add_transport_label(self):
         self.add_transport_label = Label()
-        self.add_transport_label.grid(row=0, column=1)
+        self.add_transport_label.grid(row=0, column=0, columnspan=3)
         self.add_transport_label.configure(font = "Arial 14", text = "TRANSPORT")
         
     def add_ambulance_image_label(self):
@@ -30,6 +31,7 @@ class Gui(Tk):
         self.ambulance_image_label.configure(image=self.ambulance_image,
                                              height=60,
                                              width=60)
+        self.ambulance_image_label.bind("<Button-1>", self.ambulance_clicked)
 
     def add_bike_image_label(self):
         self.bike_image_label = Label()
@@ -37,6 +39,7 @@ class Gui(Tk):
         self.bike_image_label.configure(image=self.bike_image,
                                              height=60,
                                              width=60)
+        self.bike_image_label.bind("<Button-1>", self.bike_clicked)
  
     def add_plane_image_label(self):
         self.plane_image_label = Label()
@@ -44,6 +47,16 @@ class Gui(Tk):
         self.plane_image_label.configure(image=self.plane_image,
                                              height=60,
                                              width=60)
+        self.plane_image_label.bind("<Button-1>", self.plane_clicked)
+
+    def ambulance_clicked(self, event):
+        messagebox.showinfo("Ambulance title.", "Ambulance.")
+
+    def bike_clicked(self, event):
+        messagebox.showinfo("Bike title.", "Bike.")
+    
+    def plane_clicked(self, event):
+        messagebox.showinfo("Plane title.", "Plane.")
  
 
 # Create an object of the Gui class when this module is executed
